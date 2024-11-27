ebc74fb9942565cb67dfeb791d2629c573c2c84b
AlyaaRushdy <98500365+AlyaaRushdy@users.noreply.github.com>
2024-11-27 21:35:44 +0200

    navbar and navsheet

---
+++ src/components/navbar/NavSheet.tsx
@@ -0,0 +1,51 @@
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { useState } from "react";
+import { NavLink } from "react-router-dom";
+
+function NavSheet({
+  navItems,
+}: {
+  navItems: { path: string; text: string }[];
+}) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <Sheet open={isOpen} onOpenChange={setIsOpen}>
+      <SheetHeader>
+        <SheetTitle>
+          <p className="sr-only">Navigation Menu</p>
+        </SheetTitle>
+      </SheetHeader>
+      <SheetTrigger className="inline-block md:hidden">
+        <img src="/src/assets/images/shared/icon-hamburger.svg" alt="menu" />
+      </SheetTrigger>
+      <SheetContent className="border-l-0 bg-white bg-opacity-5 pr-0 shadow-none backdrop-blur-xl">
+        <ul className="flex flex-col gap-8 pt-[7.5rem]">
+          {navItems.map((item, i) => (
+            <li
+              key={i}
+              className="font-barlow_condensed text-[16px] uppercase tracking-[2.7px] text-foreground hover:border-r-[3px] hover:border-r-white/50 has-[.active]:border-r-[3px] has-[.active]:border-r-white"
+            >
+              <NavLink
+                to={item.path}
+                onClick={() => {
+                  setIsOpen(false);
+                }}
+              >
+                <span className="pr-1 font-bold">0{i}</span> {item.text}
+              </NavLink>
+            </li>
+          ))}
+        </ul>
+      </SheetContent>
+    </Sheet>
+  );
+}
+
+export default NavSheet;
---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,39 @@
+import { Link, NavLink } from "react-router-dom";
+import NavSheet from "./NavSheet";
+import logo from "../assets/logo.svg";
+
+const navItems = [
+  { text: "Home", path: "/" },
+  { text: "Destination", path: "/destination" },
+  { text: "Crew", path: "/crew" },
+  { text: "Technology", path: "/technology" },
+];
+
+function Navbar() {
+  return (
+    <>
+      <nav className="flex flex-row items-center justify-between self-start px-6 py-6 md:pl-10 md:pr-0 md:pt-0 lg:pl-14 lg:pt-6">
+        <Link to={"/"}>
+          <img src={logo} alt="logo" />
+        </Link>
+        <div className="z-[1] hidden h-px flex-grow translate-x-12 bg-white bg-opacity-25 lg:block"></div>
+        <ul className="hidden flex-row gap-16 bg-white bg-opacity-[4%] px-32 py-9 backdrop-blur-md md:flex">
+          {navItems.map((item, i) => (
+            <li
+              key={i}
+              className="font-barlow_condensed text-[14px] uppercase tracking-[2.7px] text-foreground decoration-[3px] underline-offset-[44px] hover:underline hover:decoration-white/50 has-[.active]:underline has-[.active]:decoration-white"
+            >
+              <NavLink to={item.path}>
+                <span className="font-bold">0{i}</span> {item.text}
+              </NavLink>
+            </li>
+          ))}
+        </ul>
+
+        <NavSheet navItems={navItems} />
+      </nav>
+    </>
+  );
+}
+
+export default Navbar;
